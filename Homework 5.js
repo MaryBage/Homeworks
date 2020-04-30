4f2a84e8f0846961ceb2196dce4e4bc2376ba4bf
MaryBage <62337660+MaryBage@users.noreply.github.com>
2020-04-30 14:32:45 +0400

    Add files via upload

--- Homework 5.js
+++ Homework 5.js
@@ -1,7 +1,7 @@
 
-function bnrySrch (arr, el, i = 0, len = arr.length) {
+function bnrySrchRec (arr, el, i = 0, len = arr.length) {
 	
-	if(len - i < 1) return -1;
+	if(len - i < 0) return -1;
 	
 	mid = i + Math.round((len - i) / 2);
 	
@@ -11,9 +11,36 @@ function bnrySrch (arr, el, i = 0, len = arr.length) {
 	
 	else i = mid;
 	
-	return bnrySrch(arr, el, i, len);
+	return bnrySrchRec(arr, el, i, len);
 	
 }
 
-console.log(bnrySrch([1, 12, 15, 19, 20, 23, 27, 31, 34, 42, 45, 53, 61, 66, 68, 71, 75, 77], 68)); 
+console.log(bnrySrchRec([1, 12, 15, 19, 20, 23, 27, 31, 34, 42, 45, 53, 61, 66, 68, 71, 75, 77], 53)); 
 
+
+function bnrySrch (arr, el) {
+	
+	let i = 0;
+	
+	let len = arr.length;
+	
+	let mid = -1;
+		
+	while(len - i > 0){
+
+		mid = i + Math.round((len - i) / 2);
+		
+		if(el == arr[mid]) return mid;
+
+		if(el < arr[mid]) len = mid - 1;
+		
+		else i = mid;
+		
+	}
+	
+	return mid;
+
+	
+}
+
+console.log(bnrySrch([1, 12, 15, 19, 20, 23, 27, 31, 34, 42, 45, 53, 61, 66, 68, 71, 75, 77], 27)); 
